f31baf9bbb704a876aa88c672372d553dbb4b10d
AhmedAbdelaziz188 <111662569+AhmedAbdelaziz188@users.noreply.github.com>
2022-08-24 22:16:23 +0200

    Add files via upload

---
+++ PythonApplication2.py
@@ -0,0 +1,18 @@
+﻿print ("Hello World")
+
+#دا كدا كومنت
+#py
+x=["apple", "banana", "Cherry"]
+for name in x:
+    print(name)
+
+for ss in "banana":
+    print (ss)
+
+    Fruits=["apple", "banana", "Cherry"]
+    print (Fruits)
+
+    #z=["apple", "banana", "cherry","watermelon","peach"]
+  #  for z in range 0, 4
+    #Print(z)
+    
